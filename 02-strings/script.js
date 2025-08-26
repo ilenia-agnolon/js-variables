df33f25aa471621b770b8f4dd1a29e307c3b7728
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-08-26 14:32:20 +0200

    4. Imposta questa variabile utilizzando una proprietà // sulla variabile alphabet per ottenere la sua lunghezza

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -31,7 +31,9 @@ const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
+
+
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
